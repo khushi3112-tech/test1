136e70915b7c15320a8a6e7eed82d1a7b3a0edf9
khushirewade <khushirewade3112@gmail.com>
2026-06-29 00:43:12 +1000

    for use meld tool

--- hungry.py
+++ hungry.py
@@ -1,10 +1,5 @@
 hungry=input("are you hungry?")
 if hungry == "yes":
     print("eat samosa")
-    print("eat pizza")
-    print("eat burger")
 else:
-    thirsty=input("are you thirsty?")
-    if thirsty == "yes":
-        print("drink water")
-        print("drink soda")
+    print("do homework")
